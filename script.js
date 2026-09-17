6fbf1ec214612bb3d494cc63c7f59e5a5fbacbbe
Braeden Sy Tan <braedenjairsytan@icloud.com>
2026-09-18 06:59:02 +1200

    Added back transition and changes with js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 function startgame(mode) {
-    document.querySelector(".main-title").classList.add("hidden")
+
+    document.querySelector(".main-title").classList.add("hidden");
 
     if (mode == 1){
-        game = document.querySelector(".game-space.Casual.hidden")}
+        game = document.querySelector(".game-space.Casual.hidden");}
     else if (mode == 2){
-        game = document.querySelector(".game-space.Faithful.hidden")}
+        game = document.querySelector(".game-space.Faithful.hidden");}
     else if (mode == 3){
-            game = document.querySelector(".game-space.Worshiper.hidden")}
+            game = document.querySelector(".game-space.Worshiper.hidden");}
 
-    game.classList.remove("hidden")
-}
+    game.classList.remove("hidden");
+    
+    button = document.querySelector(".nav-button-base");
+    button.textContent = "BACK";
+    button.removeAttribute("href");
+    button.setAttribute("onclick", "backbutton()");
+
+};
+
+function backbutton(){
+    game.classList.add("hidden");
+    document.querySelector(".main-title").classList.remove("hidden");
+
+    button = document.querySelector(".nav-button-base");
+    button.textContent = "PLAY";
+    button.setAttribute("href", "index.html");
+    button.removeAttribute("onclick");
+    
+};
 
 document.querySelectorAll(".song-information ul li").forEach((item, index) => {
-    item.style.animationDelay = `${Math.sin(45 * Math.PI / 180) * (index + 1) * 50}ms`;
+    item.style.animationDelay = `${Math.sin(45 * Math.PI / 180) * (index + 1) * 25}ms`;
 });
